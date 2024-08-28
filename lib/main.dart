38671fc18c765b21868bbec98825dd66d2d97925
akkayasoft <akkayasoft@gmail.com>
2024-08-28 08:25:50 +0300

    ders3 bitti

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sevket_e_ticaret_projesi_2024/constants/app_colors.dart';
 import 'package:flutter_sevket_e_ticaret_projesi_2024/constants/theme_data.dart';
+import 'package:flutter_sevket_e_ticaret_projesi_2024/providers/theme_provider.dart';
 import 'package:flutter_sevket_e_ticaret_projesi_2024/screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +14,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'E Ticaret',
-      theme: Styles.themeData(isDarkTheme: true, context: context),
-      home: const HomeScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_){
+          return ThemeProvider();
+        }),
+      ],
+      child: Consumer<ThemeProvider>(
+        builder: (context,themeProvider,child) {
+          return MaterialApp(
+            title: 'E Ticaret',
+            theme: Styles.themeData(isDarkTheme: themeProvider.getIsDarkTheme, context: context),
+            home: const HomeScreen(),
+          );
+        }
+      ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sevket_e_ticaret_projesi_2024/providers/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider=Provider.of<ThemeProvider>(context);
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Merhaba E Ticaret Projesi'),
-            ElevatedButton(onPressed: (){}, child: Text('Proje Başlat')),
+            Text('Merhaba E Ticaret Projesi',style: TextStyle(fontSize: 30),),
+            ElevatedButton(onPressed: (){}, child: Text('Proje Başlat'),),
+            SwitchListTile(
+              title: Text(themeProvider.getIsDarkTheme?"Koyu Tema":"Açık Tema"),
+              value: themeProvider.getIsDarkTheme, onChanged: (value){
+              themeProvider.setDarkTheme(themeValue: value);
+            }),
           ],
         ),
       ),
